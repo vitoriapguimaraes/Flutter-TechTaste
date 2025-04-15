ec17b3ec16e63dc5f312db95a2afc2090c7e6320
vipistori <vipistori@gmail.com>
2025-04-15 17:51:47 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/ui/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: SplashScreen());
+  }
+}
