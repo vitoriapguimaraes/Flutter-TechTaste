16718e357cc2de5a88c89eb866192161ea607484
vipistori <vipistori@gmail.com>
2025-04-17 20:52:12 +0000

    feat/main_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/data/restaurant_data.dart';
 import 'package:myapp/ui/_core/app_theme.dart';
 import 'package:myapp/ui/splash/splash_screen.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  RestaurantData restaurantData = RestaurantData();
+  await restaurantData.getRestaurants();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            return restaurantData;
+          },
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
---
+++ lib/ui/home/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/data/categories_data.dart';
+import 'package:myapp/data/restaurant_data.dart';
+import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/ui/home/widgets/category_widget.dart';
+import 'package:myapp/ui/home/widgets/restaurant_widget.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    RestaurantData restaurantData = Provider.of<RestaurantData>(context);
+
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {}, icon: Icon(Icons.shopping_cart)),
+        ],
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24.0),
+        child: SingleChildScrollView(
+          child: Column(
+            spacing: 32.0,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: Image.asset('assets/logo.png', width: 147)),
+              Text("Boas-vindas!"),
+              TextFormField(),
+              Text("Escolha por categoria"),
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  spacing: 8.0,
+                  children: List.generate(
+                    CategoriesData.listCategories.length,
+                    (index) {
+                      return CategoryWidget(
+                        category: CategoriesData.listCategories[index],
+                      );
+                    },
+                  ),
+                ),
+              ),
+              Image.asset("assets/banners/banner_promo.png"),
+              Text("Bem avaliados"),
+              Column(
+                spacing: 16.0,
+                children: List.generate(restaurantData.listRestaurant.length, (
+                  index,
+                ) {
+                  Restaurant restaurant = restaurantData.listRestaurant[index];
+                  return RestaurantWidget(restaurant: restaurant);
+                }),
+              ),
+              SizedBox(height: 64.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
